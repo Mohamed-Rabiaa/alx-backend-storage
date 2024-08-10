2c6fff940dbd99f7644e231c72be774ae54c29bc
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-08-10 05:45:06 +0300

    Add 11-need_meeting.sql file

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,9 +1,14 @@
 -- Creates a trigger that decreases the quantity of an item after adding a new order.
 DROP TRIGGER IF EXISTS item_quantity_trigger;
+
+DELIMITER $$
+
 CREATE TRIGGER item_quantity_trigger AFTER INSERT ON orders
+For EACH ROW
 BEGIN
-        For EACH ROW
         UPDATE items
 	SET quantity = quantity - NEW.number
 	WHERE name = NEW.item_name;
-END	
+END $$
+
+DELIMITER ;
